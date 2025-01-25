a818ec2fa0715fda329762530164952266cb243b
Rahul NS <rahul9380767545@gmail.com>
2025-01-25 19:29:19 +0530

    Remove unwanted code

--- week-16-ws/src/index.ts
+++ week-16-ws/src/index.ts
@@ -4,10 +4,6 @@ const wss = new WebSocketServer({ port: 8080 });
 
 // event handler
 wss.on("connection", function(socket) {
-    console.log("user connected");
-    setInterval(() => {
-        socket.send(`Current price of solana is ${Math.random()}`);
-    }, 2000);
 
     socket.on("message", (e) => {
         if (e.toString() === "ping") {
